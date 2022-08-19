c925c892de4b1e6ce90e8966ce3aed405a6d7b15
Isabelle <isabellexavier59@gmail.com>
2022-08-19 12:08:20 -0300

    fix: fixing the right files

---
+++ script.js
@@ -0,0 +1,41 @@
+function perguntarNumeroDeCartas() {
+    numeroDeCartas = Number(prompt("Digite aqui o número de cartas (de 4 a 14, números pares)"));
+}
+
+let numeroDeCartas;
+
+perguntarNumeroDeCartas()
+
+const numerosPossiveis = [4, 6, 8, 10, 12, 14];
+
+function aoCarregar() {
+    for (i = 0; i <= 1; i++) {
+        if (numeroDeCartas === numerosPossiveis[i] && numeroDeCartas % 2 === 0) {
+            adicionarCartas();
+        } else {
+            alert("ERRO. Por favor, apenas números PARES, no intervalo entre 4 e 14.")
+            perguntarNumeroDeCartas()
+            aoCarregar()
+        }
+    }
+}
+aoCarregar()
+
+function adicionarCartas() {
+    const divCartas = document.querySelector('.cartas');
+    for (i = 0; i < numeroDeCartas; i++) {
+        let carta = `<div class="carta" onclick="virar(this)">
+                        <img src="imagens/front 1.png" alt="parrot">
+                    </div>`;
+        divCartas.innerHTML += carta
+    }
+
+}
+
+//fazer array com as divs carta
+
+
+
+function virar(divCarta) {
+    divCarta.classList.add('virada')
+}
